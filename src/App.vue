c608bfaa9aeeb2cc443b61b2ff81a1ef923a487b
diar-digits <diar@digits.id>
2022-03-09 11:47:34 +0700

    Change Color

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
           <div class="col-12">
             <form @submit.prevent="submitTodo">
               <div class="mb-3">
-                <label for="inputTodo" class="form-label fw-bold">Input To Do</label>
+                <label for="inputTodo" class="form-label fw-bold text-light">Input To Do</label>
                 <input
                   autocomplete="off"
                   placeholder="input your to do"
@@ -41,15 +41,15 @@ export default {
                   v-model="currentTask"
                 />
               </div>
-              <button type="submit" class="btn btn-primary">save</button>
+              <button type="submit" class="btn btn-light">save</button>
             </form>
           </div>
-          <div class="col-12 mt-5">
-            {{ currentTask }}
+          <div class="col-12 mt-5 text-light">
+            You"re Gonna? {{ currentTask }}
             <div class="mt-4">
               <div class="card my-2" v-for="(toDo, index) in data" :key="index">
                 <div class="card-body row">
-                  <div class="col-10 my-auto">{{ toDo }}</div>
+                  <div class="col-10 my-auto text-dark">{{ toDo }}</div>
                   <div class="col-2 mt-auto">
                     <button @click="removeTodo(index)" class="btn btn-dark ms-auto">X</button>
                   </div>
@@ -63,3 +63,8 @@ export default {
   </div>
 </template>
 
+<style>
+body {
+  background-color: #171717;
+}
+</style>
